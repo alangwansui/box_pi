a4cb773bbd64d665dd2f2a73195b6327742bb70e
alangwansui <alangwansui@gmail.com>
2016-12-21 22:19:57 +0800

    'xxx'

--- box_printer_worker.py
+++ box_printer_worker.py
@@ -1,5 +1,7 @@
 # Embedded file name: sql.py
 import oerplib
+import sys
+from  ConfigParser import RawConfigParser
 import requests
 import json
 import time
@@ -7,22 +9,86 @@ import xmlrpclib
 import logging
 import socket
 
+_logger = logging.getLogger(__name__)
+config = RawConfigParser(allow_no_value=True)
+
+def check_conf():
+    try:
+        conf_file = sys.argv[1]
+        config.read(conf_file)
+        host = config.get('box_options', 'printer_host')
+        port = config.get('box_options', 'printer_port') or '8069'
+        user = config.get('box_options', 'printer_user')
+        pw = config.get('box_options', 'printer_passwd')
+        db = config.get('box_options', 'printer_db')
+        local_printer_id = config.get('box_options', 'printer_local_ip')
+        printer_log = config.get('box_options', 'printer_log')
+        printer_time_gap = config.getfloat('box_options', 'printer_time_gap')
+        printer_pos_url = config.get('box_options', 'printer_pos_url')
+        printer_pos_timeout = config.getint('box_options', 'printer_pos_timeout')
+        printer_step_count = config.getint('box_options', 'printer_step_count')
+        print 'check conf ok'
+        return  {
+            'host':host,
+            'port':port or 8069,
+            'user':user,
+            'pw':pw,
+            'db':db,
+            'local_printer_id':local_printer_id,
+            'printer_log':printer_log,
+            'printer_time_gap': printer_time_gap or 10,
+            'printer_pos_url': printer_pos_url or 'http://127.0.0.1:8069/hw_proxy/print_xml_receipt',
+            'printer_pos_timeout': printer_pos_timeout or 30,
+            'printer_step_count': printer_step_count or 5,
+        }
+    except Exception as e:
+        print e
+        return False
+
+def run(conf):
+    post_headers = {'content-type': 'application/json'}
+    oerp = oerplib.OERP(conf['host'], protocol='xmlrpc', port=conf['port'])
+    oerp.login(conf['user'],conf['pw'], conf['db'])
+    print_obj = oerp.get('qdodoo.print.list')
+    printer_list = print_obj.search_read([('is_print', '=', False)], ['id', 'ip_addr', 'name'], limit=conf['printer_step_count']) #oerp.execute('qdodoo.print.list', 'search_read', [('is_print', '=', False)], ['id', 'ip_addr', 'name'], limit=50)
+    for pp in printer_list:
+        pp_id, content, ip_addr = pp.get('id'), pp.get('name'), pp.get('ip_addr')
+        if ip_addr:
+            lable_printer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            lable_printer.connect((ip_addr, 9100))
+            lable_printer.send(content.encode('gb2312'))
+            lable_printer.close()
+        else:
+            data = {
+                'method': 'call',
+                'params': {'receipt': content},
+                'jsonrpc': '2.0',
+                'id': 0
+            }
+            response = requests.post(conf['printer_pos_url'], data=json.dumps(data), headers=post_headers, timeout=conf['printer_pos_timeout'])
+            print response
+            ##todo  check response
+            #print_obj.write(pp_id, {'is_print': True})
+
+    print len(printer_list)
 
 
 
-oerp = oerplib.OERP('localhost', protocol='xmlrpc', port=8069)
-user = oerp.login('user', 'passwd', 'db_name')
-print(user.name)
+
+def main():
+    conf = check_conf()
+    while conf:
+        try:
+            run(conf)
+        except Exception as e:
+            time.sleep(conf['printer_time_gap'])
+            print e
+########
+main()
 
 
-# Simple 'raw' query
-user_data = oerp.execute('res.users', 'read', [user.id])
-print(user_data)
 
-# Use all methods of an OSV class
-order_obj = oerp.get('sale.order')
-order_ids = order_obj.search([])
-for order in order_obj.browse(order_ids):
+
 
 
 
